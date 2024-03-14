b1e81600f287bf652716d0c2b0ac83b9dba2a749
Rafael Lopes <rafaelsilveriodesalopes@gmail.com>
2024-03-14 13:45:05 -0300

    feat: use cors function in application

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import * as express from "express";
 import { Request, Response } from "express";
 import router from "./routes/routes";
+const cors = require("cors");
 
 const app: express.Application = express();
 const port = 8000;
 
+app.use(cors());
 app.use("/", router);
 
 app.listen(port, () => {
